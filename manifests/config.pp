14b086d6f1c387e70bbab80fb54161dcc6cdc1e1
Chadwick Banning <walkamongus@users.noreply.github.com>
2015-12-04 09:30:01 -0500

    add control of realmd.conf

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,4 +3,16 @@
 # This class is called from realmd for service config.
 #
 class realmd::config {
+
+  $_realmd_config      = $::realmd::realmd_config
+  $_realmd_config_file = $::realmd::realmd_config_file
+
+  file { $_realmd_config_file:
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+    content => template('realmd/realmd.conf.erb'),
+  }
+
 }
