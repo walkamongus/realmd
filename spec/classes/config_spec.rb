20c64a865ea50177f53bd02727281383a9ec4746
Chadwick Banning <walkamongus@users.noreply.github.com>
2015-12-07 14:51:21 -0500

    add spec tests for config class

---
+++ spec/classes/config_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe 'realmd' do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) do
+          facts
+        end
+
+        context "realmd::config class with default parameters" do
+          let(:params) {{ }}
+
+          it { should contain_file('/etc/realmd.conf').with({
+	          :owner => 'root',
+	          :group => 'root',
+	          :mode  => '0640',
+          }) }
+        end
+      end
+    end
+  end
+end
