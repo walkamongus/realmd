0e47724ad9e22ac1765ddf8748aa67eb126a1731
Chadwick Banning <walkamongus@users.noreply.github.com>
2015-12-07 14:51:05 -0500

    add parameter validations

--- manifests/init.pp
+++ manifests/init.pp
@@ -32,7 +32,49 @@ class realmd (
   $manage_krb_config       = $::realmd::params::manage_krb_config,
 ) inherits ::realmd::params {
 
-  # validate parameters here
+  if $krb_ticket_join == false {
+    if ($domain_join_user and !$domain_join_password) {
+      fail('Cannot set domain_join_user without domain_join_password')
+    }
+  }
+  if ($domain_join_password and !$domain_join_user) {
+    fail('Cannot set domain_join_password without domain_join_user')
+  }
+
+  validate_string(
+    $realmd_package_name,
+    $adcli_package_name,
+    $krb_client_package_name,
+    $sssd_package_name,
+    $sssd_service_name,
+    $domain,
+    $domain_join_user,
+    $domain_join_password,
+  )
+
+  validate_absolute_path(
+    $realmd_config_file,
+    $sssd_config_file,
+    $krb_config_file,
+  )
+
+  validate_hash(
+    $realmd_config,
+    $sssd_config,
+    $krb_config,
+  )
+
+  validate_array(
+    $mkhomedir_package_names,
+  )
+
+  validate_bool(
+    $manage_sssd_config,
+    $krb_ticket_join,
+    $manage_krb_config,
+  )
+
+  if $krb_keytab { validate_absolute_path($krb_keytab) }
 
   class { '::realmd::install': } ->
   class { '::realmd::config': } ~>
