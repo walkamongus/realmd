6e8c7ed6fbe7ba2dfec0a8359005f535b78efa54
Colin Wood <cwood06@gmail.com>
2016-07-20 16:00:58 -0700

    Need to manage and set default realm

--- manifests/join/password.pp
+++ manifests/join/password.pp
@@ -9,6 +9,20 @@ class realmd::join::password {
   $_user      = $::realmd::domain_join_user
   $_password  = $::realmd::domain_join_password
   $_join_args = $::realmd::domain_join_args
+  $_krb_config_file   = $::realmd::krb_config_file
+  $_krb_config        = $::realmd::krb_config
+  $_manage_krb_config = $::realmd::manage_krb_config
+
+  if $_manage_krb_config {
+    file { 'krb_configuration':
+      ensure  => file,
+      path    => $_krb_config_file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template('realmd/krb5.conf.erb'),
+    }
+  }
 
   exec { 'realm_join_with_password':
     path    => '/usr/bin:/usr/sbin:/bin',
