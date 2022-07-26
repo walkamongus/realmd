facb5c4873c6ef23e8546b8bda0a37cefb1d8fbc
Chris Tessmer <chris.tessmer@onyxpoint.com>
2022-07-26 19:41:09 -0400

    Better logic for realmd::manage_sssd_package

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,61 +4,50 @@
 #
 class realmd::install {
 
+  package { $::realmd::realmd_package_name:
+    ensure => $::realmd::realmd_package_ensure,
+  }
+
+  package { $::realmd::adcli_package_name:
+    ensure => $::realmd::adcli_package_ensure,
+  }
+
+  package { $::realmd::krb_client_package_name:
+    ensure => $::realmd::krb_client_package_ensure,
+  }
+
+  if $realmd::manage_sssd_package {
+    package { $::realmd::sssd_package_name:
+      ensure => $::realmd::sssd_package_ensure,
+    }
+  }
+
   case $facts['os']['name'] {
     'Ubuntu': {
       include apt
 
-      package { $::realmd::realmd_package_name:
-        ensure  => $::realmd::realmd_package_ensure,
-        require => Exec['apt_update'],
-      }
-
-      package { $::realmd::adcli_package_name:
-        ensure  => $::realmd::adcli_package_ensure,
-        require => Exec['apt_update'],
-      }
-
-      package { $::realmd::krb_client_package_name:
-        ensure  => $::realmd::krb_client_package_ensure,
-        require => Exec['apt_update'],
-      }
+      Exec['apt_update']
+      -> Package[[
+        $::realmd::realmd_package_name,
+        $::realmd::adcli_package_name,
+        $::realmd::adcli_package_name,
+        $::realmd::krb_client_package_name,
+      ]]
 
       if $realmd::manage_sssd_package {
-        package { $::realmd::sssd_package_name:
-          ensure  => $::realmd::sssd_package_ensure,
-          require => Exec['apt_update'],
-        }
+        Exec['apt_update'] -> Package[$::realmd::sssd_package_name]
       }
 
       $::realmd::required_packages.each | String $package, Hash $content | {
-        package { $package:
+        Exec['apt_update']
+        -> package { $package:
           ensure  => $content['ensure'],
-          require => Exec['apt_update'],
         }
       }
-
     }
     default: {
-      package { $::realmd::realmd_package_name:
-        ensure => $::realmd::realmd_package_ensure,
-      }
-
-      package { $::realmd::adcli_package_name:
-        ensure => $::realmd::adcli_package_ensure,
-      }
-
-      package { $::realmd::krb_client_package_name:
-        ensure => $::realmd::krb_client_package_ensure,
-      }
-
-      package { $::realmd::sssd_package_name:
-        ensure => $::realmd::sssd_package_ensure,
-      }
-
       ensure_packages($::realmd::required_packages)
     }
   }
-
-
 
 }
